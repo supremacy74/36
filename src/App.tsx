4dee6d4517bc1c6ccd97db3f53720cb85bfc9690
tidligere <103850229+supremacy74@users.noreply.github.com>
2024-05-14 18:37:50 +0600

    base

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+
+import face from './assets/face.jpg'
+import back from './assets/back.jpg'
+
+import './App.css'
+
+const App = () => {
+    const images = { face, back }
+
+    const [image, setImage] = useState(images.face)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log('test')
+            setImage((prevState) => prevState === images.face ? images.back : images.face)
+        }, 1000 * 60 * 3)
+
+        return () => clearInterval(interval)
+    }, [])
+
+    return (
+        <div className='wrapper'>
+            <img className='image' src={image} alt=""/>
+        </div>
+    )
+}
+
+export default App
